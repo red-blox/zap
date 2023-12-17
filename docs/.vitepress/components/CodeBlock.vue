5defb88f6c0b75b998a698706602926401cf2a82
Sasial <44125644+sasial-dev@users.noreply.github.com>
2023-12-17 16:14:06 +1000

    feat(codeblock): add show in playground

--- docs/.vitepress/components/CodeBlock.vue
+++ docs/.vitepress/components/CodeBlock.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="language-" :style="styles">
+		<button v-if="props.isCodeBlock && (lang === 'zapConfig' || lang === undefined)" class="tooltip" @click="showInPlayground">
+			Display in Playground
+		</button>
 		<Editor
 			:modelValue="props.code"
 			:options="{ ...(props.isCodeBlock ? CODEBLOCK_OPTIONS : EDITOR_OPTIONS), ...props.options }"
@@ -12,11 +15,14 @@
 <script setup lang="ts">
 import type monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 import { ref, watch } from 'vue';
+import { useRouter } from 'vitepress'
 
 const props = withDefaults(defineProps<{ code: string, options?: monacoEditor.editor.IStandaloneEditorConstructionOptions, lang?: string, isCodeBlock?: boolean }>(), {
 	isCodeBlock: true
 })
 defineEmits<{ (e: "update:modelValue", value: string): void }>()
+
+const { go } = useRouter()
 
 const styles = ref()
 watch(
@@ -26,14 +32,41 @@ watch(
 			width: "100%",
 			height: Math.min(code.split("\n").length * (props.options?.lineHeight ?? 18), 460) + 40 + "px",
 			padding: "20px 0px",
-			background: props.isCodeBlock ? undefined : "transparent"
+			background: props.isCodeBlock ? undefined : "transparent",
+			position: "relative",
 		};
 	},
 	{ immediate: true },
 );
 ;
 
+const showInPlayground = () => {
+	try {
+		const link = btoa(props.code)
+		go(`/playground?code=${link}`)
+	} catch (err) {
+
+	}
+}
+
 const EDITOR_OPTIONS: monacoEditor.editor.IStandaloneEditorConstructionOptions = { readOnly: true, scrollBeyondLastLine: false }
 const CODEBLOCK_OPTIONS: monacoEditor.editor.IStandaloneEditorConstructionOptions = { ...EDITOR_OPTIONS, minimap: { enabled: false }, lineNumbers: "off", scrollbar: { vertical: "hidden", horizontal: "hidden", handleMouseWheel: false }, overviewRulerLanes: 0  }
 </script>
 
+<style>
+.tooltip {
+	position: absolute;
+	z-index: 10;
+	background: var(--vp-c-default-soft);
+	border-radius: 8px;
+	padding: 4px 8px;
+	right: 12px;
+	top: 12px;
+	opacity: 0;
+	transition: opacity 0.3s;
+}
+
+.language-:hover > .tooltip {
+	opacity: 1
+}
+</style>
