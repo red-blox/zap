e9a65b667a096402a118ec5f71d7fc398ebaf3be
xSwezan <101433236+xSwezan@users.noreply.github.com>
2024-01-14 18:29:59 +0100

    On returns disconnect function
    Zap.Event.On() returns a function to disconnect the listener

--- zap/src/output/luau/server.rs
+++ zap/src/output/luau/server.rs
@@ -645,10 +645,18 @@ impl<'a> ServerOutput<'a> {
 			self.push_ty(ty);
 		}
 
-		self.push(") -> ())\n");
+		self.push(") -> ()): () -> ()\n");
 		self.indent();
 
 		self.push_line(&format!("table.insert(events[{id}], {callback})"));
+		
+		self.push_line("return function()");
+		self.indent();
+		
+		self.pushline(&format!("table.remove(events[{id}], table.find(events[{id}], Callback))"));
+			
+		self.dedent();
+		self.push_line("end");
 
 		self.dedent();
 		self.push_line("end,");
