2144876b57105019cc9fde2b0fbbb4cbb81308ba
daimond113 <contact@daimond113.com>
2025-03-02 01:14:25 +0100

    Default min to 0

--- zap/src/config.rs
+++ zap/src/config.rs
@@ -428,11 +428,7 @@ impl Range {
 	}
 
 	pub fn numty(&self) -> Option<NumTy> {
-		let (Some(min), Some(max)) = (self.min, self.max) else {
-			return None;
-		};
-
-		Some(NumTy::from_f64(min, max))
+		Some(NumTy::from_f64(self.min.unwrap_or_default(), self.max?))
 	}
 }
 
