bfb0b3ef7314943283f6f638c7a96cefc496a022
daimond113 <contact@daimond113.com>
2025-04-10 21:53:41 +0200

    Correct conditions

--- zap/src/output/luau/server.rs
+++ zap/src/output/luau/server.rs
@@ -544,7 +544,7 @@ impl<'a> ServerOutput<'a> {
 			));
 			let last = format!("incoming_ids[{}]", id + 1);
 			self.push_line(&format!(
-				"if {last} and order_id < {last} and {last} - order_id > {} then",
+				"if {last} and (order_id <= {last} or {last} - order_id > {}) then",
 				(UNRELIABLE_ORDER_NUMTY.max() / 2.0).floor(),
 			));
 			self.indent();
