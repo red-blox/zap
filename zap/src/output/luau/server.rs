b932e5fc0baad6c7c1d12abb43be3b7d18f6801b
Ezzenix <anton.lampinen@gmail.com>
2026-05-02 14:59:10 +0200

    Dont send events to disconnected players

--- zap/src/output/luau/server.rs
+++ zap/src/output/luau/server.rs
@@ -518,6 +518,8 @@ impl<'src> ServerOutput<'src> {
 			self.push_line(&format!("task.spawn(function(player_2, call_id_2, {args})"));
 			self.indent();
 
+			self.push_line("if player_map[player_2] == nil then return end");
+
 			self.push_line(&format!(
 				"local {rets_string} = reliable_events[{server_id}](player_2, {args})"
 			));
@@ -830,6 +832,8 @@ impl<'src> ServerOutput<'src> {
 		self.push(")\n");
 		self.indent();
 
+		self.push_line("if player_map[player] == nil then return end");
+
 		if self.config.include_profile_labels {
 			self.push_line(&format!("debug.profilebegin(\"{} Fire\")", ev.display_path()));
 		}
@@ -1085,6 +1089,7 @@ impl<'src> ServerOutput<'src> {
 				self.push_line("local buff, used, inst = outgoing_buff, outgoing_used, outgoing_inst");
 				self.push_line(&format!("for _, player in {list} do"));
 				self.indent();
+				self.push_line("if player_map[player] == nil then continue end");
 				self.push_line("load_player(player)");
 				self.push_line("alloc(used)");
 				self.push_line("buffer.copy(outgoing_buff, outgoing_apos, buff, 0, used)");
@@ -1102,6 +1107,7 @@ impl<'src> ServerOutput<'src> {
 				}
 				self.push_line(&format!("for _, player in {list} do"));
 				self.indent();
+				self.push_line("if player_map[player] == nil then continue end");
 				if ordered {
 					self.push_unreliable_order_id("player", ev.id);
 				}
@@ -1164,6 +1170,7 @@ impl<'src> ServerOutput<'src> {
 				self.push_line("local buff, used, inst = outgoing_buff, outgoing_used, outgoing_inst");
 				self.push_line(&format!("for player in {set} do"));
 				self.indent();
+				self.push_line("if player_map[player] == nil then continue end");
 				self.push_line("load_player(player)");
 				self.push_line("alloc(used)");
 				self.push_line("buffer.copy(outgoing_buff, outgoing_apos, buff, 0, used)");
@@ -1181,6 +1188,7 @@ impl<'src> ServerOutput<'src> {
 				}
 				self.push_line(&format!("for player in {set} do"));
 				self.indent();
+				self.push_line("if player_map[player] == nil then continue end");
 				if ordered {
 					self.push_unreliable_order_id("player", ev.id);
 				}
@@ -1604,6 +1612,18 @@ impl<'src> ServerOutput<'src> {
 		self.dedent();
 		self.push_line("end");
 		self.push("\n");
+		self.push_line("for _, player in Players:GetPlayers() do");
+		self.indent();
+		self.push_line("load_player(player)");
+		self.push_line("player_map[player] = save()");
+		self.dedent();
+		self.push_line("end");
+		self.push_line("Players.PlayerAdded:Connect(function(player)");
+		self.indent();
+		self.push_line("load_player(player)");
+		self.push_line("player_map[player] = save()");
+		self.dedent();
+		self.push_line("end)");
 		self.push_line("Players.PlayerRemoving:Connect(function(player)");
 		self.indent();
 		self.push_line("player_map[player] = nil");
